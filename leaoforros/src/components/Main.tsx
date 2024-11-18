7ff09525b010221c46dfa6b3556ba615b4fe3e7a
Bianca Leão <biancapeleao@gmail.com>
2024-11-18 12:54:34 -0300

    tsx

--- leaoforros/src/components/Main.tsx
+++ leaoforros/src/components/Main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import '../estilo/main.scss';
 import Projects from './Projects';
@@ -7,21 +8,27 @@ import carrosselImg1 from '../img/carrossel.png';
 import carrosselImg2 from '../img/carrossel2.png';
 import carrosselImg3 from '../img/carrossel3.jpeg';
 
-const slides = [
+interface Slide {
+  img: string;
+  alt: string;
+}
+
+const slides: Slide[] = [
   { img: carrosselImg1, alt: 'Slide 1' },
   { img: carrosselImg2, alt: 'Slide 2' },
   { img: carrosselImg3, alt: 'Slide 3' },
 ];
 
 const Main = () => {
-
+  
   const [slideIndex, setSlideIndex] = useState(0);
 
   const proximoSlide = () => {
     setSlideIndex((prevIndex) => (prevIndex + 1) % slides.length);
   };
+
   useEffect(() => {
-    const intervalId = setInterval(proximoSlide, 3000); 
+    const intervalId = setInterval(proximoSlide, 3000);
     return () => clearInterval(intervalId); 
   }, []);
 
@@ -36,12 +43,12 @@ const Main = () => {
       </div>
 
       <div className="main__about poly-regular" id="main__about">
-      A Leão Forros e Divisórias é uma empresa especializada em soluções para ambientes corporativos e residenciais, oferecendo produtos de alta qualidade e design personalizado. Com mais de 18 anos de experiência, garantimos conforto, estética e funcionalidade para transformar seus espaços com excelência e confiança.
+        A Leão Forros e Divisórias é uma empresa especializada em soluções para ambientes corporativos e residenciais, oferecendo produtos de alta qualidade e design personalizado. Com mais de 18 anos de experiência, garantimos conforto, estética e funcionalidade para transformar seus espaços com excelência e confiança.
       </div>
 
-      <section className="main__projects" id='main__projects'>
-        <Projects/>
-        <Clients/>
+      <section className="main__projects" id="main__projects">
+        <Projects />
+        <Clients />
       </section>
     </main>
   );
